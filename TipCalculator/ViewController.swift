5f9bcaf2c13c675c7d27e236023f6da00d1bf1e0
RainyGuo <guo312@purdue.edu>
2016-12-17 22:51:08 -0500

    init local submit of tipcalc

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipControl.addTarget(self, action: #selector(ViewController.calculateTip(_:)), for: UIControlEvents.valueChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // let: const
+        let tipArr=[0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipArr[tipControl.selectedSegmentIndex]
+        let total = bill+tip
+        print(String(format: "%.2f", tip))
+        tipLabel.text=String(format: "$%.2f", tip)
+        totalLabel.text=String(format: "$%.2f", total)
+    }
 
 }
 
